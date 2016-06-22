c61328c51e12b0523ab001317ca54aaacbb4fdd9
bean-vip <bean_vip@126.com>
2016-06-22 17:14:16 +0800

    Spidertest

--- java/spider_test/src/main/java/com/x/demo/main/ITuring.java
+++ java/spider_test/src/main/java/com/x/demo/main/ITuring.java
@@ -3,6 +3,7 @@ package com.x.demo.main;
 import java.io.IOException;
 import java.util.HashMap;
 
+import com.x.demo.util.Constant;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
@@ -13,40 +14,51 @@ public class ITuring {
 
     public static void main(String[] args) {
 
-        String url = "https://wanwang.aliyun.com/domain/searchresult/?keyword=gorobby&suffix=.com";
-        getUrlResult(url);
+        int ca = 0;
 
-//        for(int i = 97; i <= 122; i++) {
-//            String tmp = String.valueOf((char)i);
-//            decur(tmp);
-//        }
+        System.out.println("start...");
+        String url = "gorobbybean1";
+        getUrlResult(url, Constant.COM);
+
+        for(int i = 97; i <= 122; i++) {
+            String tmp = String.valueOf((char)i);
+            System.out.println("ca : " + (++ca));
+
+            decur(tmp);
+        }
+        System.out.println("end!!!");
     }
 
-
     public static void decur(String s) {
-        if(s.length() > 2) {
+        if(s.length() == Constant.YL) {
             return;
         }
         String res = "";
         for(int i = 97; i <= 122; i++) {
             res = s + String.valueOf((char)i);
-            System.out.println(res);
+//            System.out.println(res);
+            if (Constant.YL == res.length()) {
+                getUrlResult(res, Constant.COM);
+            }
             decur(res);
         }
     }
 
-    public static String getUrlResult(String url) {
+    public static String getUrlResult(String url, String topFlag) {
         // TODO Auto-generated method stub
 //		String url = "http://www.ituring.com.cn/tupubarticle/5567";
 //        String url = "http://sg.godaddy.com/zh/domains/searchresults.aspx?checkAvail=1&domainToCheck=gorobbybean";
+
+        String fullUrl = Constant.FIRST_URL + url + topFlag + Constant.LAST_URL;
+
         try {
             HashMap<String, String> dataMap = new HashMap<String, String>();
             dataMap.put("top", "4");
-            Document doc = Jsoup.connect(url).header("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36").data(dataMap).ignoreContentType(true).timeout(20*1000).post();
+            Document doc = Jsoup.connect(fullUrl).header("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36").data(dataMap).ignoreContentType(true).timeout(20*1000).post();
 //            System.out.println(doc.html());
             String resHTml = doc.html();
-            System.out.println(resHTml);
-            if (resHTml.contains("已售出")) {
+//            System.out.println(resHTml);
+            if (resHTml.contains(":1")) {
                 System.out.println(url);
             }
             return doc.html();
---
+++ java/spider_test/src/main/java/com/x/demo/util/Constant.java
@@ -0,0 +1,18 @@
+package com.x.demo.util;
+
+/**
+ * Date : 2016-06-22
+ */
+public interface Constant {
+
+    String FIRST_URL = "https://checkapi.aliyun.com/check/checkdomain?callback=jQuery111102884474361781507_1466582494335&domain=";
+
+    String LAST_URL = "&token=check-web-hichina-com%3Asfkeke07tj1q94zstbv8vxmx392mcyv9&_=1466582494336";
+
+    String COM = ".com";
+
+    int YL = 4;
+
+
+    String XINNET = "http://checkdomain.xinnet.com/domainCheck?callbackparam=jQuery172040868125354321105_1466586402285&searchRandom=7&prefix=52binge&suffix=.com&_=1466586408959";
+}
