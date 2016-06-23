a263da0c05a4db0c4cd22ce8c80e793f3a9e730b
bean-vip <bean_vip@126.com>
2016-06-23 16:48:36 +0800

    add constant

--- java/x/micro_ana/src/main/java/com/x/demo/util/Constant.java
+++ java/x/micro_ana/src/main/java/com/x/demo/util/Constant.java
@@ -4,13 +4,6 @@ import java.text.SimpleDateFormat;
 
 public interface Constant {
 
-// // for test
-//    String MICRO_POINT_INDEX = "micro_point";
-//    String MICRO_POINT_TYPE = "micro_point";
-//    String ElasticSearchIPs = "192.168.181.190,192.168.181.196,192.168.181.198";
-//    int MaxEsSearchCnt = 10000000;
-
-
     String CREATE_DT = "create_dt";
 
     String SHOP_ID = "shop_id";
