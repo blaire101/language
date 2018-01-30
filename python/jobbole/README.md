0ced2ad366cd038fb89632c7db24291ad0f812da
blair <blair.values@gmail.com>
2018-01-30 15:08:24 +0800

    rename tools to _source

--- python/jobbole/README.md
+++
@@ -1,5 +0,0 @@
-# Python爬虫入门
-
-[python.jobbole.com][1]
-
-[1]: http://python.jobbole.com/81336/
--- python/jobbole/demo.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-
-import urllib2
- 
-response = urllib2.urlopen("http://www.baidu.com")
-print response.read()
